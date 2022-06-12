3cd3f70b2a5bf56a0a69a8e51d2f380d3fc48a49
Andrew Maurer <andmaurer13@gmail.com>
2022-06-12 12:11:56 -0400

    finished test page, deploying to tfsection

--- js/tfCardTest.js
+++ js/tfCardTest.js
@@ -14,134 +14,75 @@ const MPKButton = document.querySelector("#MPK");
 
 // second choice buttons above
 const secondaryChoices = document.querySelectorAll('.secButtons');
+console.log("querySelectors");
 // const clearSelect = document.querySelector("#resPage");
 // abvoe is my attempt at creating a togglable selection
 
 
-kneeButton.addEventListener("click", () => showKnees());
-console.log("hello");
-console.log("hello");
+// kneeButton.addEventListener("click", () => showKnees());
+kneeButton.addEventListener("click", () => toggleSelectionsFlex("secButtons"));
+kneeButton.addEventListener("click", () => toggleSelectionsBlock("nav"));
 
-feetButton.addEventListener("click", () => showExtFriction());
-linerButton.addEventListener("click", () => showExtFricManChoice());
-componentButton.addEventListener("click", () => showComponents());
+// feetButton.addEventListener("click", () => showExtFriction());
+// linerButton.addEventListener("click", () => showExtFricManChoice());
+// componentButton.addEventListener("click", () => showComponents());
 // Main event handlers above
 
+
+
 kOnekTwoButton.addEventListener("click", () => showkOnekTwo());
+kOnekTwoButton.addEventListener("click", () => toggleSelectionsFlex("kOne"));
+
+
 singleAxisButton.addEventListener("click", () => showSingleAxis());
+singleAxisButton.addEventListener("click", () => toggleSelectionsBlock("singleAxis"));
+
+
 safeKneeButton.addEventListener("click", () => showSafetyKnee());
+safeKneeButton.addEventListener("click", () => toggleSelectionsBlock("safety"));
+
+
 polyKneeButton.addEventListener("click", () => showPolycentricKnee());
+polyKneeButton.addEventListener("click", () => toggleSelectionsBlock("polycentric"));
+
 MPKButton.addEventListener("click", () => showMPK());
+console.log("EventHandlers");
 
-
-// second event handlers buttons above
+// secondary event handlers buttons above
 
 
 
 // The above event handler will remove the selected section, reverting the sections to initial values, without reloading.
 
 
-function showKnees() {
-console.log("Show 1")
-  document.getElementById("kOneTwo").classList.add('secButtons');
-  document.getElementById("singleAxis").classList.add('secButtons');
-  document.getElementById("safeKnee").classList.add('secButtons');
-  document.getElementById("polyKnee").classList.add('secButtons');
-  document.getElementById("MPK").classList.add('secButtons');
-  document.getElementById("kneeButton").classList.add('clear');
 
+function toggleSelectionsFlex(name) {
+  console.log("Show 1");
+  let e = document.getElementsByClassName(name);
+     Array.from(e).forEach((x) => {
+      if (x.style.display === "flex") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "flex";
+      }
+    })
+  }
 
-  const clearKneesButton = document.querySelector(".clear");
-  clearKneesButton.addEventListener("click", () => clearKnees());
-
-        function clearKnees() {
-            console.log("Clear Knee Cards");
-            document.getElementById("kOneTwo").style.cssText = "display: inherited;"
-            document.getElementById("singleAxis").style.cssText = "display: inherited;"
-            document.getElementById("safeKnee").style.cssText = "display: inherited;"
-            document.getElementById("polyKnee").style.cssText = "display: inherited;"
-            document.getElementById("MPK").style.cssText = "display: inherited;"
-            document.getElementById("kneeButton").classList.add('nextClass');
-            document.getElementById("kneeButton").classList.remove('clear');
-
-
-            const newClass = document.querySelector(".nextClass");
-            newClass.addEventListener("click", () => nextClassFun());
-
-            function nextClassFun() {
-                document.getElementById("kOneTwo").classList.add('secButtons');
-                document.getElementById("singleAxis").classList.add('secButtons');
-                document.getElementById("safeKnee").classList.add('secButtons');
-                document.getElementById("polyKnee").classList.add('secButtons');
-                document.getElementById("MPK").classList.add('secButtons');
-                document.getElementById("kneeButton").classList.add('clear');
-
-                console.log("Show Knee Cards Inner");
-                document.getElementById("kOneTwo").style.cssText = "display: flex;";
-                document.getElementById("singleAxis").style.cssText = "display: flex;";
-                document.getElementById("safeKnee").style.cssText = "display: flex;";
-                document.getElementById("polyKnee").style.cssText = "display: flex;";
-                document.getElementById("MPK").style.cssText = "display: flex;";
-                showKneesOne();
-            }
-
-
-
+  function toggleSelectionsBlock(name) {
+    console.log("Show 1");
+    let e = document.getElementsByClassName(name);
+       Array.from(e).forEach((x) => {
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
         }
-
-  console.log("Show Knee Cards");
-  document.getElementById("kOneTwo").style.cssText = "display: flex;"
-  document.getElementById("singleAxis").style.cssText = "display: flex;"
-  document.getElementById("safeKnee").style.cssText = "display: flex;"
-  document.getElementById("polyKnee").style.cssText = "display: flex;"
-  document.getElementById("MPK").style.cssText = "display: flex;"
-
-  }
-
-  function showKneesOne() {
-    console.log("Show 2")
-      document.getElementById("kOneTwo").classList.add('secButtons');
-      document.getElementById("singleAxis").classList.add('secButtons');
-      document.getElementById("safeKnee").classList.add('secButtons');
-      document.getElementById("polyKnee").classList.add('secButtons');
-      document.getElementById("MPK").classList.add('secButtons');
-      document.getElementById("kneeButton").classList.add('clear');
-      document.getElementById("kneeButton").classList.remove('clear');
-      document.getElementById("kneeButton").classList.remove('nextClass');
-
-    
+      })
+    }
 
 
-      console.log("Show Knee One Cards");
-      document.getElementById("kOneTwo").style.cssText = "display: flex;"
-      document.getElementById("singleAxis").style.cssText = "display: flex;"
-      document.getElementById("safeKnee").style.cssText = "display: flex;"
-      document.getElementById("polyKnee").style.cssText = "display: flex;"
-      document.getElementById("MPK").style.cssText = "display: flex;"
-
-  }
 
 
-//   $(document).ready(function(){
-//     $(".mainChoice").click(function(){
-//       $(".collapse").collapse('toggle');
-//     });
-//     });
-
-    // $('.mainChoice').each(function(){
-    //     if ($(this).hasClass('secButtons')) {
-    //         $(this).collapse('toggle');
-    //     }
-    // });
-
-// function clearKnees() {
-//     console.log("Clear Knee Cards");
-//     document.getElementById("kOneTwo").style.cssText = "display: none;"
-//     document.getElementById("singleAxis").style.cssText = "display: none;"
-//     document.getElementById("safeKnee").style.cssText = "display: none;"
-//     document.getElementById("polyKnee").style.cssText = "display: none;"
-//     document.getElementById("MPK").style.cssText = "display: none;"
-// }
 
   function showkOnekTwo() {
     console.log("Show K1/K2");
@@ -167,6 +108,143 @@ console.log("Show 1")
     console.log("Show MPK's");
 
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function showKnees() {
+//   console.log("Show 1")
+  // document.getElementById("kOneTwo").classList.add('secButtons');
+  // document.getElementById("singleAxis").classList.add('secButtons');
+  // document.getElementById("safeKnee").classList.add('secButtons');
+  // document.getElementById("polyKnee").classList.add('secButtons');
+  // document.getElementById("MPK").classList.add('secButtons');
+  // document.getElementById("kneeButton").classList.add('clear');
+
+
+  // const clearKneesButton = document.querySelector(".clear");
+  // clearKneesButton.addEventListener("click", () => clearKnees());
+
+  //       function clearKnees() {
+  //           console.log("Clear Knee Cards");
+  //           document.getElementById("kOneTwo").style.cssText = "display: inherited;"
+  //           document.getElementById("singleAxis").style.cssText = "display: inherited;"
+  //           document.getElementById("safeKnee").style.cssText = "display: inherited;"
+  //           document.getElementById("polyKnee").style.cssText = "display: inherited;"
+  //           document.getElementById("MPK").style.cssText = "display: inherited;"
+  //           document.getElementById("kneeButton").classList.add('nextClass');
+  //           document.getElementById("kneeButton").classList.remove('clear');
+
+
+  //           const newClass = document.querySelector(".nextClass");
+  //           newClass.addEventListener("click", () => nextClassFun());
+
+  //           function nextClassFun() {
+  //               document.getElementById("kOneTwo").classList.add('secButtons');
+  //               document.getElementById("singleAxis").classList.add('secButtons');
+  //               document.getElementById("safeKnee").classList.add('secButtons');
+  //               document.getElementById("polyKnee").classList.add('secButtons');
+  //               document.getElementById("MPK").classList.add('secButtons');
+  //               document.getElementById("kneeButton").classList.add('clear');
+
+  //               console.log("Show Knee Cards Inner");
+  //               document.getElementById("kOneTwo").style.cssText = "display: flex;";
+  //               document.getElementById("singleAxis").style.cssText = "display: flex;";
+  //               document.getElementById("safeKnee").style.cssText = "display: flex;";
+  //               document.getElementById("polyKnee").style.cssText = "display: flex;";
+  //               document.getElementById("MPK").style.cssText = "display: flex;";
+  //               showKneesOne();
+  //           }
+
+
+
+  //       }
+
+  // console.log("Show Knee Cards");
+  // document.getElementById("kOneTwo").style.cssText = "display: flex;"
+  // document.getElementById("singleAxis").style.cssText = "display: flex;"
+  // document.getElementById("safeKnee").style.cssText = "display: flex;"
+  // document.getElementById("polyKnee").style.cssText = "display: flex;"
+  // document.getElementById("MPK").style.cssText = "display: flex;"
+
+  // }
+
+  // function showKneesOne() {
+  //   console.log("Show 2")
+  //     document.getElementById("kOneTwo").classList.add('secButtons');
+  //     document.getElementById("singleAxis").classList.add('secButtons');
+  //     document.getElementById("safeKnee").classList.add('secButtons');
+  //     document.getElementById("polyKnee").classList.add('secButtons');
+  //     document.getElementById("MPK").classList.add('secButtons');
+  //     document.getElementById("kneeButton").classList.add('clear');
+  //     document.getElementById("kneeButton").classList.remove('clear');
+  //     document.getElementById("kneeButton").classList.remove('nextClass');
+
+    
+
+
+  //     console.log("Show Knee One Cards");
+  //     document.getElementById("kOneTwo").style.cssText = "display: flex;"
+  //     document.getElementById("singleAxis").style.cssText = "display: flex;"
+  //     document.getElementById("safeKnee").style.cssText = "display: flex;"
+  //     document.getElementById("polyKnee").style.cssText = "display: flex;"
+  //     document.getElementById("MPK").style.cssText = "display: flex;"
+
+  // }
+
+
+//   $(document).ready(function(){
+//     $(".mainChoice").click(function(){
+//       $(".collapse").collapse('toggle');
+//     });
+//     });
+
+    // $('.mainChoice').each(function(){
+    //     if ($(this).hasClass('secButtons')) {
+    //         $(this).collapse('toggle');
+    //     }
+    // });
+
+// function clearKnees() {
+//     console.log("Clear Knee Cards");
+//     document.getElementById("kOneTwo").style.cssText = "display: none;"
+//     document.getElementById("singleAxis").style.cssText = "display: none;"
+//     document.getElementById("safeKnee").style.cssText = "display: none;"
+//     document.getElementById("polyKnee").style.cssText = "display: none;"
+//     document.getElementById("MPK").style.cssText = "display: none;"
+// }
+
+
 
   
 
