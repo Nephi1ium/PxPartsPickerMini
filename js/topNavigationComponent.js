a511b96b19faf8a590ed0fce0ce9a2179f1ade64
Andrew Maurer <andmaurer13@gmail.com>
2022-10-04 22:09:52 -0400

    worked on nav, changed and removed as mush bootstrap as possible

--- js/topNavigationComponent.js
+++ js/topNavigationComponent.js
@@ -3,13 +3,13 @@
         const hnav =
             `<nav class="allNav">
                 <nav class="fullSizeNav">
-                    <a href="../index.html" class="nav-item" >Home</a>
+                    <a href="homePage.html" class="nav-item" >Home</a>
                     <a href="px-picker-tfpin.html" class="nav-item" >TransFemoral Pin</a>
                     <a href="px-picker-tfsuction.html" class="nav-item" id="">TransFemoral Suction</a>
                     <a href="px-picker-ttpin.html" class="nav-item" id="">TransTibial Pin</a>
                     <a href="px-picker-ttsuction.html" class="nav-item" id="">TransTibial Suction</a>
-                    <a href="px-picker-ttsuction.html" class="nav-item" id="">PDF</a>
-                    <a href="px-picker-ttsuction.html" class="nav-item" id="">Virtual Care</a>
+                    <a href="pdf.html" class="nav-item" id="">PDF</a>
+                    <a href="virtual-care-document.html" class="nav-item" id="">Virtual Care</a>
 
                 </nav>
 
@@ -22,8 +22,8 @@
                     <a href="px-picker-tfsuction.html" class="mobileNavigationBut" id="TFSucBut">TransFemoral Suction</a>
                     <a href="px-picker-ttpin.html" class="mobileNavigationBut" id="TTPinBut">TransTibial Pin</a>
                     <a href="px-picker-ttsuction.html" class="mobileNavigationBut" id="TTSucBut">TransTibial Suction</a>
-                    <a href="px-picker-ttsuction.html" class="mobileNavigationBut" id="PDFBut">PDF</a>
-                    <a href="px-picker-ttsuction.html" class="mobileNavigationBut" id="VCareBut">Virtual Care</a>
+                    <a href="pdf.html" class="mobileNavigationBut" id="PDFBut">PDF</a>
+                    <a href="virtual-care-document.html" class="mobileNavigationBut" id="VCareBut">Virtual Care</a>
                 </nav>
             </nav>`;
             document.getElementById("mainNavigation").innerHTML = hnav;
