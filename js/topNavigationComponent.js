168564921b0d6f922363c77376b236ba6352f102
Andrew Maurer <andmaurer13@gmail.com>
2024-07-22 21:51:32 -0400

    Removed items and gitignored items unneeded

--- js/topNavigationComponent.js
+++ js/topNavigationComponent.js
@@ -5,8 +5,6 @@
                 <nav class="fullSizeNav">
                     <a href="homePage.html" class="nav-item" >Home</a>
                     <a href="px-picker-kitselection.html" class="nav-item" id="">Kit Selection</a>
-                    <a href="pdf.html" class="nav-item" id="">PDF</a>
-                    <a href="virtual-care-document.html" class="nav-item" id="">Virtual Care</a>
                     <a href="notes-guide.html" class="nav-item" id="">Notes Guide and Templates</a>
 
 
@@ -21,12 +19,6 @@
                         </li>
                         <li class="navListItem">
                             <a href="px-picker-kitselection.html" class="mobileNavigationBut" id="kitSeleBut">Kit Selection</a>
-                        </li>
-                        <li class="navListItem">
-                            <a href="pdf.html" class="mobileNavigationBut" id="PDFBut">PDF</a>
-                        </li>
-                        <li class="navListItem">
-                            <a href="virtual-care-document.html" class="mobileNavigationBut" id="VCareBut">Virtual Care</a>
                         </li>
                         <li class="navListItem">
                         <a href="notes-guide.html" class="mobileNavigationBut" id="notesTemplateBut">Notes Guide and Templates</a>
@@ -47,15 +39,11 @@
             console.log("Displaying Main Navigator");
             const showHome = document.getElementById("homeButt");
             const showKitSelect = document.getElementById("kitSeleBut");
-            const showPDF = document.getElementById("PDFBut");
-            const showVCare = document.getElementById("VCareBut");
             const showNotes = document.getElementById("notesTemplateBut");
 
 
             showHome.classList.toggle('showMobNavItems');
             showKitSelect.classList.toggle('showMobNavItems');
-            showPDF.classList.toggle('showMobNavItems');
-            showVCare.classList.toggle('showMobNavItems');
             showNotes.classList.toggle('showMobNavItems');
 
 
